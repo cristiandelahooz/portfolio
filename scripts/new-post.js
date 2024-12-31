6a6e53aeb82d9d03b98e150cef49c30b5e172f88
saratrasv <cyrondelahoz@gmail.com>
2024-12-31 11:11:58 -0400

    chore: commit save point

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -1,7 +1,7 @@
 /* This is a script to create a new post markdown file with front-matter */
 
-import fs from 'fs';
-import path from 'path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 
 function getDate() {
 	const today = new Date();
